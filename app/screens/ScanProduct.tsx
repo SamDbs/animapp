8600320fd1c942876db39f4b67a4cfcda5f1a612
Leandre Daumont <1338620+digiz3d@users.noreply.github.com>
2021-03-14 19:15:38 +0100

    Set barcode scanner up

--- app/screens/ScanProduct.tsx
+++ app/screens/ScanProduct.tsx
@@ -1,12 +1,52 @@
-import React from 'react'
-import { SafeAreaView } from 'react-native'
+import { BarCodeScanner } from 'expo-barcode-scanner'
+import { StyleSheet, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
 
 import { Text } from '../components/Themed'
 
-export default function ScanProduct() {
+export default function ScanProduct(): JSX.Element {
+  const [hasPermission, setHasPermission] = useState<boolean | null>(null)
+
+  useEffect(() => {
+    async function checkCameraPermission() {
+      const { status } = await BarCodeScanner.requestPermissionsAsync()
+      setHasPermission(status === 'granted')
+    }
+    checkCameraPermission()
+  }, [])
+
+  if (hasPermission === null) {
+    return (
+      <View style={style.screen}>
+        <Text>Waiting for permission...</Text>
+      </View>
+    )
+  }
+
+  if (hasPermission === false) {
+    return (
+      <View style={style.screen}>
+        <Text>Waiting for permission...</Text>
+      </View>
+    )
+  }
+
   return (
-    <SafeAreaView>
-      <Text>SCAN ICI</Text>
-    </SafeAreaView>
+    <View style={style.screen}>
+      <BarCodeScanner
+        onBarCodeScanned={(scan) => console.log('scanned:', scan)}
+        style={style.viewFinder}
+      />
+    </View>
   )
 }
+
+const style = StyleSheet.create({
+  screen: {
+    flexGrow: 1,
+  },
+  viewFinder: {
+    flexGrow: 1,
+    justifyContent: 'flex-end',
+  },
+})
