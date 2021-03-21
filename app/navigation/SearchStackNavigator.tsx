90c9465967b0fb220744127ef9b7e062e4e2dd0f
Leandre Daumont <1338620+digiz3d@users.noreply.github.com>
2021-03-21 17:17:03 +0100

    handle search loading

--- app/navigation/SearchStackNavigator.tsx
+++ app/navigation/SearchStackNavigator.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import React from 'react'
 
 import { SearchStackParamList } from '../types'
 import SearchProductsIngredients from '../screens/SeachProductsIngredients'
@@ -9,9 +10,11 @@ const Stack = createStackNavigator<SearchStackParamList>()
 
 export default function SearchStackNavigator(): JSX.Element {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen component={SearchProductsIngredients} name="Search" />
-      <Stack.Screen component={Product} name="Product" />
-    </Stack.Navigator>
+    <SafeAreaProvider>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen component={SearchProductsIngredients} name="Search" />
+        <Stack.Screen component={Product} name="Product" />
+      </Stack.Navigator>
+    </SafeAreaProvider>
   )
 }
--- app/screens/SeachProductsIngredients.tsx
+++ app/screens/SeachProductsIngredients.tsx
@@ -7,6 +7,7 @@ import useSWR from 'swr'
 
 import { Text } from '../components/Themed'
 import { SearchStackParamList } from '../types'
+import Loader from '../components/Loader'
 
 type ProductScreenNavigationProp = StackNavigationProp<SearchStackParamList, 'Product'>
 
@@ -14,37 +15,50 @@ type Props = {
   navigation: ProductScreenNavigationProp
 }
 
-const SearchInputContainer = (props: any) => <View style={style.searchInputContainer} {...props} />
+function SearchInputContainer(props: any) {
+  return <View style={style.searchInputContainer} {...props} />
+}
 
-const SearchResultProduct = (props: {
+function SearchResultProduct(props: {
   productId: string
   isLast: boolean
   navigate?: Props['navigation']['push']
   children: JSX.Element
-}) => (
-  <TouchableWithoutFeedback
-    style={[style.result, props.isLast && style.noBorder].filter(Boolean)}
-    onPress={
-      props.navigate
-        ? () => props.navigate && props.navigate('Product', { productId: props.productId })
-        : undefined
-    }>
-    {props.children}
-  </TouchableWithoutFeedback>
-)
+}) {
+  return (
+    <TouchableWithoutFeedback
+      style={[style.result, props.isLast && style.noBorder].filter(Boolean)}
+      onPress={
+        props.navigate
+          ? () => props.navigate && props.navigate('Product', { productId: props.productId })
+          : undefined
+      }>
+      {props.children}
+    </TouchableWithoutFeedback>
+  )
+}
 
-const SearchResultIngredient = (props: { isLast: boolean; children: JSX.Element }) => (
-  <TouchableWithoutFeedback style={[style.result, props.isLast && style.noBorder].filter(Boolean)}>
-    {props.children}
-  </TouchableWithoutFeedback>
-)
+function SearchResultIngredient(props: { isLast: boolean; children: JSX.Element }) {
+  return (
+    <TouchableWithoutFeedback
+      style={[style.result, props.isLast && style.noBorder].filter(Boolean)}>
+      {props.children}
+    </TouchableWithoutFeedback>
+  )
+}
+
+function Center(props: { children: JSX.Element }): JSX.Element {
+  return <View style={style.center}>{props.children}</View>
+}
 
 export default function SearchProductsIngredients({
   navigation: { navigate },
 }: Props): JSX.Element {
   const [input, setInput] = useState('')
-  const willFetch = input.length > 3
-  const { data } = useSWR(willFetch ? `/search?q=${input}` : null)
+  const shouldFetch = input.length > 3
+  const { data, error } = useSWR(shouldFetch ? `/search?q=${input}` : null)
+  const loading = shouldFetch && !data && !error
+  const empty = shouldFetch && error
 
   return (
     <SafeAreaView style={style.page}>
@@ -57,8 +71,23 @@ export default function SearchProductsIngredients({
           clearButtonMode="always"
         />
       </SearchInputContainer>
+      {!shouldFetch && (
+        <Center>
+          <Text>Please type something</Text>
+        </Center>
+      )}
+      {loading && (
+        <Center>
+          <Loader />
+        </Center>
+      )}
+      {empty && (
+        <Center>
+          <Text>No result</Text>
+        </Center>
+      )}
       <ScrollView>
-        {data ? (
+        {data && (
           <>
             {data.products.map((result: any, i: number) => (
               <SearchResultProduct
@@ -75,10 +104,6 @@ export default function SearchProductsIngredients({
               </SearchResultIngredient>
             ))}
           </>
-        ) : (
-          <View style={style.noResults}>
-            <Text>No result yet</Text>
-          </View>
         )}
       </ScrollView>
     </SafeAreaView>
@@ -103,6 +128,10 @@ const style = StyleSheet.create({
   },
   noResults: {
     flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  center: {
     justifyContent: 'center',
     alignItems: 'center',
   },
