995b3f71a2744d3497d99dad0b87fda201f39d0f
SamDbs <dbs.samantha@gmail.com>
2021-03-25 14:46:55 +0100

    add CRUD Faq

---
+++ api/src/views/faq.ts
@@ -0,0 +1,34 @@
+import Faq from '../models/faq'
+import Language from '../models/language'
+import FaqTranslation from '../models/faqTranslation'
+
+export function viewFaqs(faqs: Faq[], language: Language['id'] = 'FR') {
+  return faqs.map((faq) => viewFaq(faq, language))
+}
+
+export function viewFaq(faq: Faq, language: Language['id'] = 'FR') {
+  let faqTranslation = faq.translations.find((t) => t.languageId === language)
+
+  if (!faqTranslation) faqTranslation = faq.translations.find((t) => t.languageId === 'FR')
+
+  const faqClient = {
+    id: faq.id,
+    question: faqTranslation?.question ?? 'This question is not translated yet',
+    answer: faqTranslation?.answer ?? 'This answer is not translated yet',
+  }
+  return faqClient
+}
+
+export function viewFaqWithTranslations(faq: Faq) {
+  const faqTranslations = faq.translations
+
+  const faqAdmin = {
+    id: faq.id,
+    translations: faqTranslations,
+  }
+  return faqAdmin
+}
+
+export function viewFaqTranslation(faqTranslation: FaqTranslation) {
+  return faqTranslation
+}
