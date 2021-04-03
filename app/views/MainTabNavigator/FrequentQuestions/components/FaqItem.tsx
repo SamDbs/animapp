43102b7c540902ff95de8998888e9a1b12d25462
Léandre Daumont <1338620+digiz3d@users.noreply.github.com>
2021-04-03 17:42:43 +0200

    [-] App - Animated FAQ items. (#10)

--- app/views/MainTabNavigator/FrequentQuestions/components/FaqItem.tsx
+++ app/views/MainTabNavigator/FrequentQuestions/components/FaqItem.tsx
@@ -1,19 +1,54 @@
-import React, { useState } from 'react'
-import { Text, View } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
+import { Animated, Easing, StyleSheet, Text, View } from 'react-native'
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler'
 
 export type FaqItemType = { id: number; question: string; answer: string }
 
 type Props = { item: FaqItemType }
 
+function Title(props: JSX.Element['props']) {
+  return <Text style={style.title}>{props.children}</Text>
+}
 export default function FaqItem(props: Props): JSX.Element {
-  const [expanded, setExpanded] = useState(false)
+  const [height, setHeight] = useState(0)
+  const [target, setTarget] = useState(1)
+  const animation = useRef(new Animated.Value(1)).current
+
+  useEffect(() => {
+    setTarget(0)
+  }, [])
+
+  useEffect(() => {
+    Animated.timing(animation, {
+      toValue: target,
+      duration: 250,
+      useNativeDriver: false,
+      easing: Easing.linear,
+    }).start()
+  }, [animation, target])
+
   return (
-    <View>
-      <TouchableWithoutFeedback onPress={() => setExpanded(!expanded)}>
-        <Text>{props.item.question}</Text>
+    <View style={style.component}>
+      <TouchableWithoutFeedback onPressIn={() => setTarget(target === 0 ? 1 : 0)}>
+        <Title>{props.item.question}</Title>
       </TouchableWithoutFeedback>
-      {expanded && <Text>{props.item.answer}</Text>}
+      <Animated.View
+        onLayout={(e) => height === 0 && setHeight(e.nativeEvent.layout.height)}
+        style={
+          height !== 0
+            ? {
+                maxHeight: animation.interpolate({ inputRange: [0, 1], outputRange: [0, height] }),
+                overflow: 'scroll',
+              }
+            : undefined
+        }>
+        <Text>{props.item.answer}</Text>
+      </Animated.View>
     </View>
   )
 }
+
+const style = StyleSheet.create({
+  component: { padding: 10 },
+  title: { fontSize: 16, fontWeight: 'bold', marginBottom: 10 },
+})
--- app/views/MainTabNavigator/FrequentQuestions/index.tsx
+++ app/views/MainTabNavigator/FrequentQuestions/index.tsx
@@ -5,11 +5,15 @@ import useSWR from 'swr'
 
 import FaqItem, { FaqItemType } from './components/FaqItem'
 
+function Title(props: JSX.Element['props']) {
+  return <Text style={{ fontSize: 20, margin: 10 }}>{props.children}</Text>
+}
 export default function FrequentQuestions(): JSX.Element {
   const { data: faqs, error } = useSWR(`/faq`)
   const isLoading = !faqs && !error
   return (
     <SafeAreaView>
+      <Title>FAQ</Title>
       {isLoading && <ActivityIndicator size={40} color="#ccc" />}
       {error && <ActivityIndicator size={40} color="#ccc" />}
       {faqs && faqs.map((item: FaqItemType) => <FaqItem key={item.id} item={item} />)}
