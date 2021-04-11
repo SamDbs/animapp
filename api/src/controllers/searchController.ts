f3d47196f3fe539ef386a59b589ef3a552f04891
SamDbs <dbs.samantha@gmail.com>
2021-04-11 18:31:28 +0200

    change searchBar

--- api/src/controllers/searchController.ts
+++ api/src/controllers/searchController.ts
@@ -2,33 +2,43 @@ import { RequestHandler } from 'express'
 import { ILike } from 'typeorm'
 
 import { viewProducts } from '../views/product'
-import { viewIngredients } from '../views/ingredient'
 import Product from '../models/product'
-import IngredientTranslation from '../models/ingredientTranslation'
 import { NotFoundError } from '../middleware/errorHandler'
+import Brand from '../models/brand'
+import IngredientTranslation from '../models/ingredientTranslation'
 
 export const searchAll: RequestHandler = async (req, res) => {
   const { language, q } = req.query
   const products = await Product.find({
     where: { name: ILike(`%${q}%`) },
-    relations: ['translations'],
+    relations: ['translations', 'brand'],
   })
+
+  const brands = await Brand.find({
+    where: { name: ILike(`%${q}%`) },
+    relations: ['products', 'products.translations', 'products.brand'],
+  })
+
+  if (!products.length && !brands.length) {
+    throw new NotFoundError()
+  }
+  res.json({
+    products: viewProducts(
+      [...products, ...brands.map((brand) => brand.products).flat()],
+      language?.toString().toUpperCase(),
+    ),
+  })
+}
+export const searchByIngredients: RequestHandler = async (req, res) => {
+  const { language, q } = req.query
+  const queryDeleteParenthesis = req.query.q?.toString().replace(/\(.*\)/Ugms, '')
+  const queryWord = queryDeleteParenthesis?.split(',').map((item) => item.trim())
+  const queryWord2 = queryDeleteParenthesis?.split(';').map((item) => item.trim())
+  const queryWord3 = queryDeleteParenthesis?.split('\n').map((item) => item.trim())
 
   const ingredients = await IngredientTranslation.createQueryBuilder('t')
     .innerJoinAndSelect('t.ingredient', 'ingredient')
     .leftJoinAndSelect('ingredient.translations', 'ts')
     .where('t.name ILike :q', { q: `%${q}%` })
     .getMany()
-
-  if (!products.length && !ingredients.length) {
-    throw new NotFoundError()
-  }
-
-  res.json({
-    ingredients: viewIngredients(
-      ingredients.map((i) => i.ingredient),
-      language?.toString().toUpperCase(),
-    ),
-    products: viewProducts(products, language?.toString().toUpperCase()),
-  })
 }
