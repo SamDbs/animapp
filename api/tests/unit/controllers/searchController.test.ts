dba05ea757dcd2ad31084dbbd97c8b99f1fe0b6d
SamDbs <dbs.samantha@gmail.com>
2021-04-13 23:43:47 +0200

    changing test

--- api/tests/unit/controllers/searchController.test.ts
+++ api/tests/unit/controllers/searchController.test.ts
@@ -5,6 +5,6 @@ describe('search multiple ingredients', () => {
     const stringTest = `small, petits pois (pois), poisson (arete, 
       chair)`
     const tableauMots = parseQueryToWordArray(stringTest)
-    expect(tableauMots).toEqual(expect.arrayContaining(['small', 'petits pois', 'poisson']))
+    expect(tableauMots).toEqual(['small', 'petits pois', 'poisson'])
   })
 })
