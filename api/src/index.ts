23ccd370a2d7376f52d6aa621841af4f2f34ba11
SamDbs <dbs.samantha@gmail.com>
2021-04-25 16:42:22 +0200

    heroku

--- api/src/index.ts
+++ api/src/index.ts
@@ -19,7 +19,12 @@ import { errorHandler } from './middleware/errorHandler'
 const PORT = ((process.env.PORT as unknown) as number) || 8080
 const HOST = '0.0.0.0'
 if (process.env.DATABASE_URL) {
-  createConnection({ url: process.env.DATABASE_URL, type: 'postgres', ssl: true })
+  createConnection({
+    url: process.env.DATABASE_URL,
+    type: 'postgres',
+    ssl: true,
+    extra: { ssl: { rejectUnauthorized: false } },
+  })
 } else {
   createConnection()
 }
