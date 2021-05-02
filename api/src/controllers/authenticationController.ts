3be4601081314fb42208affe25cc9dd3d3037ab9
Leandre Daumont <1338620+digiz3d@users.noreply.github.com>
2021-05-02 17:06:24 +0200

    fix login when wrong password

--- api/src/controllers/authenticationController.ts
+++ api/src/controllers/authenticationController.ts
@@ -2,14 +2,16 @@ import { compare } from 'bcryptjs'
 import { RequestHandler } from 'express'
 import { sign } from 'jsonwebtoken'
 
+import { NotAuthorizedError } from '../middleware/errorHandler'
 import Admin from '../models/admin'
 
 export const authenticate: RequestHandler = async (req, res) => {
   const admin = await Admin.findOneOrFail({ where: { login: req.body.login } })
   const passwordIsCorrect = await compare(req.body.password, admin.password)
-  if (passwordIsCorrect) {
-    const jwt = sign(admin.id.toString(), process.env.AUTH_SECRET_KEY as string)
-    res.json({ jwt })
-    return
-  }
+
+  if (!passwordIsCorrect) throw new NotAuthorizedError()
+
+  const jwt = sign(admin.id.toString(), process.env.AUTH_SECRET_KEY as string)
+  res.json({ jwt })
+  return
 }
