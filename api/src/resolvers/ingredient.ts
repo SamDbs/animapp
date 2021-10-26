87cffc2462d361adfa1ca7c62f8debd4cd169db8
Léandre Daumont <digiz3d@users.noreply.github.com>
2021-10-27 00:30:16 +0200

    improve ingredients-management

--- api/src/resolvers/ingredient.ts
+++ api/src/resolvers/ingredient.ts
@@ -5,12 +5,15 @@ import {
   Field,
   FieldResolver,
   Info,
+  InputType,
+  Int,
   Mutation,
   Query,
   Resolver,
   Root,
 } from 'type-graphql'
 import { GraphQLResolveInfo } from 'graphql'
+import { FindManyOptions, FindOperator, In, IsNull, Not } from 'typeorm'
 
 import Ingredient from '../models/ingredient'
 import IngredientTranslation from '../models/ingredientTranslation'
@@ -22,21 +25,82 @@ class UpdateIngredientArgs implements Partial<Ingredient> {
   rating?: number
 }
 
+@InputType()
+class IngredientFilters {
+  @Field(() => Boolean)
+  deleted?: boolean
+}
+
+@ArgsType()
+class GetIngredientsArgs {
+  @Field(() => Int, { nullable: true })
+  limit?: number
+
+  @Field(() => Int, { nullable: true })
+  offset?: number
+
+  @Field({ nullable: true })
+  searchTerms?: string
+
+  @Field({ nullable: true })
+  filters?: IngredientFilters
+}
+
 @Resolver(() => Ingredient)
 export default class IngredientResolver {
   @Query(() => Ingredient)
-  ingredient(
-    @Arg('id') id: string,
-    @Info() info: GraphQLResolveInfo,
-  ): ReturnType<typeof Ingredient['findOne']> {
-    return Ingredient.findOne(id, {
+  ingredient(@Arg('id') id: string, @Info() info: GraphQLResolveInfo): Promise<Ingredient> {
+    return Ingredient.findOneOrFail(id, {
       select: getSelectedFieldsFromForModel(info, Ingredient),
     })
   }
 
   @Query(() => [Ingredient])
-  ingredients(): ReturnType<typeof Ingredient['find']> {
-    return Ingredient.find()
+  async ingredients(@Args() args: GetIngredientsArgs): Promise<Ingredient[]> {
+    const deletedAt = args.filters?.deleted === true ? Not(IsNull()) : IsNull()
+    const options: FindManyOptions<Ingredient> = {
+      order: { id: 'ASC' },
+      where: { deletedAt },
+      withDeleted: true,
+    }
+    if (args.limit) options.take = args.limit
+    if (args.limit && args.offset) options.skip = args.offset
+    if (args.searchTerms) {
+      const ingredientIds = await IngredientTranslation.find({
+        select: ['ingredientId'],
+        where: [
+          { name: new FindOperator('ilike', `%${args.searchTerms}%`), languageId: 'EN' },
+          { review: new FindOperator('ilike', `%${args.searchTerms}%`), languageId: 'EN' },
+          { description: new FindOperator('ilike', `%${args.searchTerms}%`), languageId: 'EN' },
+        ],
+      })
+      console.log('ingre', ingredientIds)
+      // Object.assign(options.where, { id: In(ingredientIds.map((x) => x.ingredientId)) })
+    }
+
+    return Ingredient.find(options)
+  }
+
+  @Query(() => Int)
+  async ingredientsCount(@Args() args: GetIngredientsArgs): Promise<number> {
+    const deletedAt = args.filters?.deleted === true ? Not(IsNull()) : IsNull()
+    const options: FindManyOptions<Ingredient> = {
+      order: { id: 'ASC' },
+      where: { deletedAt },
+      withDeleted: true,
+    }
+    if (args.searchTerms) {
+      const faqIds = await IngredientTranslation.find({
+        select: ['ingredientId'],
+        where: [
+          { name: new FindOperator('ilike', `%${args.searchTerms}%`), languageId: 'EN' },
+          { review: new FindOperator('ilike', `%${args.searchTerms}%`), languageId: 'EN' },
+          { description: new FindOperator('ilike', `%${args.searchTerms}%`), languageId: 'EN' },
+        ],
+      })
+      options.where = { id: In(faqIds.map((x) => x.ingredientId)) }
+    }
+    return Ingredient.count(options)
   }
 
   @FieldResolver(() => String, { nullable: true })
@@ -44,7 +108,27 @@ export default class IngredientResolver {
     const ingredientTranslation = await IngredientTranslation.findOne({
       where: { ingredientId: ingredient.id, languageId: 'EN' },
     })
-    return ingredientTranslation?.name
+    return ingredientTranslation?.name ?? '-'
+  }
+
+  @FieldResolver(() => String, { nullable: true })
+  async review(
+    @Root() ingredient: Ingredient,
+  ): Promise<IngredientTranslation['review'] | undefined> {
+    const ingredientTranslation = await IngredientTranslation.findOne({
+      where: { ingredientId: ingredient.id, languageId: 'EN' },
+    })
+    return ingredientTranslation?.review ?? '-'
+  }
+
+  @FieldResolver(() => String, { nullable: true })
+  async description(
+    @Root() ingredient: Ingredient,
+  ): Promise<IngredientTranslation['description'] | undefined> {
+    const ingredientTranslation = await IngredientTranslation.findOne({
+      where: { ingredientId: ingredient.id, languageId: 'EN' },
+    })
+    return ingredientTranslation?.description ?? '-'
   }
 
   @Mutation(() => Ingredient)
@@ -55,7 +139,7 @@ export default class IngredientResolver {
 
   @Mutation(() => Ingredient)
   async updateIngredient(
-    @Arg('id') id: number,
+    @Arg('id') id: string,
     @Args() update: UpdateIngredientArgs,
   ): Promise<Ingredient> {
     if (typeof update.rating === 'number') {
@@ -66,5 +150,20 @@ export default class IngredientResolver {
 
     await Ingredient.update(id, update)
     return Ingredient.findOneOrFail(id)
+  }
+
+  @Mutation(() => String)
+  async deleteIngredient(@Arg('id') id: string) {
+    const ingredient = await Ingredient.findOneOrFail(id)
+    ingredient.softRemove()
+    return id
+  }
+
+  @Mutation(() => String)
+  async restoreIngredient(@Arg('id') id: string) {
+    const ingredient = await Ingredient.findOneOrFail(id, { withDeleted: true })
+    ingredient.deletedAt = null
+    await ingredient.save()
+    return ingredient.id
   }
 }
